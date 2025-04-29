eecb82439bcd381839188a0f414e939f3a71d0b2
Johcel Gene T. Bitara • BS IT <jbitara@gbox.adnu.edu.ph>
2025-04-30 01:06:12 +0800

    chore: added server boilerplate.

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+
+server.listen(PORT, () => {
+  console.log(`server running at http://${HOSTNAME}:${PORT}`);
+});
